1d641d7153ad270ae2db69523c459790c9944182
leskem92 <leskem@gmail.com>
2016-11-29 14:42:54 +0100

    upload search

--- Search/Form1.Designer.cs
+++ Search/Form1.Designer.cs
@@ -28,12 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.text_File = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button_SelectFolder = new System.Windows.Forms.Button();
+            this.button_Search = new System.Windows.Forms.Button();
+            this.list_FilesFound = new System.Windows.Forms.ListBox();
+            this.combo_Directory = new System.Windows.Forms.ComboBox();
+            this.combo_extension = new System.Windows.Forms.ComboBox();
+            this.textBox_SelectedFolder = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // text_File
+            // 
+            this.text_File.Location = new System.Drawing.Point(74, 4);
+            this.text_File.Name = "text_File";
+            this.text_File.Size = new System.Drawing.Size(100, 20);
+            this.text_File.TabIndex = 0;
+            this.text_File.TextChanged += new System.EventHandler(this.text_File_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Look for:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "File extension";
+            // 
+            // button_SelectFolder
+            // 
+            this.button_SelectFolder.Location = new System.Drawing.Point(212, 4);
+            this.button_SelectFolder.Name = "button_SelectFolder";
+            this.button_SelectFolder.Size = new System.Drawing.Size(126, 23);
+            this.button_SelectFolder.TabIndex = 4;
+            this.button_SelectFolder.Text = "Select folder...";
+            this.button_SelectFolder.UseVisualStyleBackColor = true;
+            this.button_SelectFolder.Click += new System.EventHandler(this.button_SelectFolder_Click);
+            // 
+            // button_Search
+            // 
+            this.button_Search.Location = new System.Drawing.Point(212, 35);
+            this.button_Search.Name = "button_Search";
+            this.button_Search.Size = new System.Drawing.Size(126, 26);
+            this.button_Search.TabIndex = 5;
+            this.button_Search.Text = "Search";
+            this.button_Search.UseVisualStyleBackColor = true;
+            this.button_Search.Click += new System.EventHandler(this.button_Search_Click);
+            this.button_Search.Enter += new System.EventHandler(this.button_Search_Click);
+            // 
+            // list_FilesFound
+            // 
+            this.list_FilesFound.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.list_FilesFound.FormattingEnabled = true;
+            this.list_FilesFound.Location = new System.Drawing.Point(12, 104);
+            this.list_FilesFound.Name = "list_FilesFound";
+            this.list_FilesFound.Size = new System.Drawing.Size(709, 225);
+            this.list_FilesFound.TabIndex = 6;
+            this.list_FilesFound.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.list_FilesFound_MouseDoubleClick);
+            // 
+            // combo_Directory
+            // 
+            this.combo_Directory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.combo_Directory.FormattingEnabled = true;
+            this.combo_Directory.Location = new System.Drawing.Point(12, 67);
+            this.combo_Directory.Name = "combo_Directory";
+            this.combo_Directory.Size = new System.Drawing.Size(661, 21);
+            this.combo_Directory.TabIndex = 7;
+            this.combo_Directory.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.combo_Directory_MouseDoubleClick);
+            // 
+            // combo_extension
+            // 
+            this.combo_extension.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.combo_extension.FormattingEnabled = true;
+            this.combo_extension.IntegralHeight = false;
+            this.combo_extension.Items.AddRange(new object[] {
+            ".txt",
+            ".pdf",
+            ".doc",
+            ".ppt",
+            ".xls"});
+            this.combo_extension.Location = new System.Drawing.Point(74, 40);
+            this.combo_extension.Name = "combo_extension";
+            this.combo_extension.Size = new System.Drawing.Size(121, 21);
+            this.combo_extension.TabIndex = 8;
+            // 
+            // textBox_SelectedFolder
+            // 
+            this.textBox_SelectedFolder.Location = new System.Drawing.Point(355, 9);
+            this.textBox_SelectedFolder.Name = "textBox_SelectedFolder";
+            this.textBox_SelectedFolder.Size = new System.Drawing.Size(366, 20);
+            this.textBox_SelectedFolder.TabIndex = 9;
+            this.textBox_SelectedFolder.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(733, 345);
+            this.Controls.Add(this.textBox_SelectedFolder);
+            this.Controls.Add(this.combo_extension);
+            this.Controls.Add(this.combo_Directory);
+            this.Controls.Add(this.list_FilesFound);
+            this.Controls.Add(this.button_Search);
+            this.Controls.Add(this.button_SelectFolder);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.text_File);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Search";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox text_File;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button_SelectFolder;
+        private System.Windows.Forms.Button button_Search;
+        private System.Windows.Forms.ListBox list_FilesFound;
+        private System.Windows.Forms.ComboBox combo_Directory;
+        private System.Windows.Forms.ComboBox combo_extension;
+        private System.Windows.Forms.TextBox textBox_SelectedFolder;
     }
 }
 
--- Search/Form1.cs
+++ Search/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Search
 {
@@ -14,7 +15,117 @@ namespace Search
     {
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();            
+        }
+
+        string selectedFolder;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+
+        }
+
+        private void button_SelectFolder_Click(object sender, EventArgs e)
+        {
+            var folderDialog = new FolderBrowserDialog();
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectedFolder = folderDialog.SelectedPath;
+                    // folderDialog.SelectedPath -- your result
+                    textBox_SelectedFolder.Text = selectedFolder.ToString();
+                }
+            }
+        }
+
+        private void button_Search_Click(object sender, EventArgs e)
+        {
+            list_FilesFound.Items.Clear();
+            text_File.Enabled = false;
+            button_SelectFolder.Enabled = false;
+            button_Search.Text = "Searching ...";
+            DirSearch(selectedFolder);
+            
+            button_Search.Text = "Search";
+            text_File.Enabled = true;
+            button_SelectFolder.Enabled = true;
+        }
+
+       
+        void DirSearch(string sDir)
+        {
+            try
+            {
+                foreach (string d in Directory.GetDirectories(sDir))
+                {                    
+                    foreach (string f in Directory.GetFiles(d))
+                    {
+                        //MessageBox.Show(f.ToLower().Substring(d.Count() + 1));
+                        if (!(combo_extension.SelectedItem == null))
+                        {
+                            if (f.ToLower().Substring(d.Count() + 1).Contains(text_File.Text.ToLower().Trim()) && f.ToLower().Substring(d.Count() + 1).Contains(combo_extension.SelectedItem.ToString()))
+
+                                {
+                                    combo_Directory.Items.Add(d.Substring(sDir.Count() + 1));
+                                    list_FilesFound.Items.Add(f);
+                                }
+                        }
+
+                        else if (f.ToLower().Substring(d.Count() + 1).Contains(text_File.Text.ToLower().Trim()))
+                        {
+                            combo_Directory.Items.Add(d.Substring(sDir.Count() + 1));
+                            list_FilesFound.Items.Add(f);
+                        }
+                                                  
+                    }
+                    DirSearch(d);
+                }
+
+                //unique list of folder in combo_Directory
+                HashSet<string> uniqueListFolder = new HashSet<string>();
+                foreach (var item in combo_Directory.Items)
+                {
+                    uniqueListFolder.Add(item.ToString());
+                }
+                
+                combo_Directory.Items.Clear();
+
+                foreach (var item in uniqueListFolder)
+                {
+                    combo_Directory.Items.Add(item);
+                }
+                
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+
+       
+	
+
+        private void text_File_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void list_FilesFound_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            System.Diagnostics.Process.Start(list_FilesFound.SelectedItem.ToString());
+        }
+
+        private void combo_Directory_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            System.Diagnostics.Process.Start(combo_Directory.SelectedItem.ToString());
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
-}
+
+        
+    }
